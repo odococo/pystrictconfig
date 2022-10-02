c8f18435dd235844ebf189461169fa3b327a3dd9
odococo <ivan.lamperti.work@gmail.com>
2022-10-02 22:46:36 +0200

    Added script to upload to pypi

---
+++ upload_pypi.py
@@ -0,0 +1,25 @@
+import shutil
+
+import click
+from bumpver.cli import update
+from build.__main__ import main as build
+from twine.commands.check import main as check
+from twine.commands.upload import main as upload
+
+
+@click.command()
+@click.option('-u', '--username', prompt=True, envvar='TWINE_USERNAME')
+@click.option('-p', '--password', prompt=True, envvar='TWINE_PASSWORD')
+@click.option('--to-pypi/--no-to-pypi', default=True)
+def main(username: str, password: str, to_pypi: bool):
+    shutil.rmtree('dist', ignore_errors=True)
+    update(['--patch'])
+    if to_pypi:
+        build([])
+        check(['dist/*'])
+        upload([f'-u {username}', f'-p {password}', 'dist/*'])
+        print('Uploaded to PyPi')
+
+
+if __name__ == '__main__':
+    main()
